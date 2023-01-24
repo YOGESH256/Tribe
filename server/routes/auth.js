4316063382f5eb2cdc07b694008dc7584ab54c90
YOGESH256 <yogeshkhatri2565@gmail.com>
2023-01-25 00:18:22 +0530

    added assignment

---
+++ server/routes/auth.js
@@ -0,0 +1,9 @@
+import express, { Router } from 'express';
+import { login } from '../controllers/auth.js'
+
+
+const router = express.Router();
+
+router.post("/login", login);
+
+export default router;
